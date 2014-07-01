9189b508d5342d510369871efd941ccf2656b95b
ProfNandaa <profnandaa@gmail.com>
2014-07-01 12:14:05 +0300

    small modif.

--- form.php
+++ form.php
@@ -11,7 +11,7 @@
       echo "\n</pre>";
     }
     ?>
-    <form action="/form" method="post">
+    <form action="/forms" method="post">
       <div><textarea name="content" rows="3" cols="60"></textarea></div>
       <div><input type="submit" value="Sign Guestbook"></div>
     </form>
